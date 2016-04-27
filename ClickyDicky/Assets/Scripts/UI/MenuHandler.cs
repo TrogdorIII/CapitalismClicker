d5d8ea9b80224f8eb65fbe8cd72ef5242f873763
jarrahholt <1002738@student.sae.edu.au>
2016-04-27 17:41:44 +1000

    Yeah Boiz no worries

---
+++ ClickyDicky/Assets/Scripts/UI/MenuHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class MenuHandler : MonoBehaviour {
+
+	public bool isTrumpMode;
+
+	public AudioSource audio;
+	public AudioClip register;
+
+	public Text trumpModeBtn;
+	public GameObject trumpParticles;
+	public GameObject moneyParticles;
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void LoadScene()
+	{
+		audio.PlayOneShot (register);
+		Application.LoadLevel (1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+	public void TrumpMode()
+	{
+		if (isTrumpMode == false) {
+			isTrumpMode = true;
+			trumpParticles.SetActive (true);
+			moneyParticles.SetActive (false);
+			trumpModeBtn.text = "Trump Mode: ON";
+		} else {
+			isTrumpMode = false;
+			trumpParticles.SetActive (false);
+			moneyParticles.SetActive (true);
+			trumpModeBtn.text = "Trump Mode: OFF";
+		}
+	}
+}
