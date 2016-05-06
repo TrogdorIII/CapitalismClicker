d6874b11aae57de2ecadca210414530b7db085aa
Christian McFarlane <1002183@student.sae.edu.au>
2016-05-06 16:29:32 +1000

    Went over some of the transforms and fixed up anchor points, added basic as hell desaturation script

---
+++ ClickyDicky/Assets/Scripts/UI/DesaturateAll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DesaturateAll : MonoBehaviour
+{
+    public Image[] imagesInScene;
+    void OnEnable()
+    {
+        imagesInScene = (Image[])GameObject.FindObjectsOfType(typeof(Image)); //returns Image[]
+    }
+
+    public void AdjustGreyscale(float value)
+    {
+        Color colour;
+        float greyVal;
+        for (int i = 0; i < imagesInScene.Length; i++)
+        {
+            colour = imagesInScene[i].color;
+            greyVal = ((colour.r + colour.g + colour.b) / 3);
+            imagesInScene[i].color = Color.Lerp(imagesInScene[i].color, new Color(greyVal, greyVal, greyVal, colour.a), value);
+        }
+    }
+}
